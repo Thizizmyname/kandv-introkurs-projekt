838994f350437b0646f5746c0209b55d4b270078
Per Engström <per.olov.engstrom@gmail.com>
2015-10-07 19:51:10 +0200

    Added the Village cards

--- thunderstoneCards.py
+++ thunderstoneCards.py
@@ -1,21 +1,41 @@
-# Class = [(cardtype, cardname, [list of type], [list of dependencies], Magic atk, Disease)]
+# Class = [(cardname, [list of type], [list of dependencies], Magic atk, Disease)]
 
 
 heroes = [
-    ('Chalice', ['fighter', 'cleric'], ['disease'], False, False),
-    ('Redblade', ['fighter', 'thief'], [], False, False),
-    ('Outlands', ['fighter'], [], False, False),
-    ('Feayn', ['fighter', 'archer'], [], False, False),
-    ('Regian', ['cleric'], [], True, False),
-    ('Dwarf', ['fighter'], [], False, False),
-    ('Selurin', ['wizard'], [], True, False),
-    ('Elf', ['wizard'], [], True, False),
-    ('Amazon', ['fighter', 'archer'], [], False, False),
-    ('Lorigg', ['thief'], [], False, False),
-    ('Thyrian', ['fighter'], [], False, False)
-    ]
+        ('Chalice', ['fighter', 'cleric'], ['disease'], False, False),
+        ('Redblade', ['fighter', 'thief'], [], False, False),
+        ('Outlands', ['fighter'], [], False, False),
+        ('Feayn', ['fighter', 'archer'], [], False, False),
+        ('Regian', ['cleric'], [], True, False),
+        ('Dwarf', ['fighter'], [], False, False),
+        ('Selurin', ['wizard'], [], True, False),
+        ('Elf', ['wizard'], [], True, False),
+        ('Amazon', ['fighter', 'archer'], [], False, False),
+        ('Lorigg', ['thief'], [], False, False),
+        ('Thyrian', ['fighter'], [], False, False)
+        ]
 
 monsters = []
 
 
-villagers = []
+villagers = [
+        ('Flaming Sword', ['weapon', 'edged'], [], True, False),
+        ('Arcane Energies', ['spell'], [], True, False),
+        ('Short Sword', ['weapon', 'edged'], [], True, False),
+        ('Spear', ['weapon', 'edged'], [], False, False),
+        ('Fireball', ['spell'], [], True, False),
+        ('Trainer', ['villager'], [], False, False),
+        ('Town Guard', ['villager'], [], False, False),
+        ('Battle Fury', ['spell'], [], False, False),
+        ('Banish', ['spell'], [], False, False),
+        ('Magical Aura', ['spell'], [], False, False),
+        ('Lightstone Gem', ['item', 'light', 'magic'], [], False, False),
+        ('Feast', ['item', 'food'], [], False, False),
+        ('Goodberries', ['item', 'food', 'magic'], [], True, False),
+        ('Hatchet', ['weapon', 'edged'], [], False, False),
+        ('Pawnbroker', ['villager'], [], False, False),
+        ('Barkeep', ['villager'], [], False, False),
+        ('Lantern', ['item', 'light'], [], False, False),
+        ('Warhammer', ['weapon', 'blunt'], [], False, False),
+        ('Polearm', ['weapon', 'edged'], [], False, False)
+        ]
