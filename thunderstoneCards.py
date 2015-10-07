613e971771ce1dd32989aa50ab86c29da533aca0
Alexis Remmers <alexis.remmers@gmail.com>
2015-10-07 19:13:08 +0200

    Added all Hero-cards

---
+++ thunderstoneCards.py
@@ -0,0 +1,21 @@
+# Class = [(cardtype, cardname, [list of type], [list of dependencies], Magic atk, Disease)]
+
+
+heroes = [
+    ('Chalice', ['fighter', 'cleric'], ['disease'], False, False),
+    ('Redblade', ['fighter', 'thief'], [], False, False),
+    ('Outlands', ['fighter'], [], False, False),
+    ('Feayn', ['fighter', 'archer'], [], False, False),
+    ('Regian', ['cleric'], [], True, False),
+    ('Dwarf', ['fighter'], [], False, False),
+    ('Selurin', ['wizard'], [], True, False),
+    ('Elf', ['wizard'], [], True, False),
+    ('Amazon', ['fighter', 'archer'], [], False, False),
+    ('Lorigg', ['thief'], [], False, False),
+    ('Thyrian', ['fighter'], [], False, False)
+    ]
+
+monsters = []
+
+
+villagers = []
